03355e1f6cadb11eaf76c3babd79ab322cc54927
muhid <shaikhmuhid165@gmail.com>
2023-09-23 01:21:33 +0530

    done with something on page 2

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 
 const http = require('http');
+const fs = require('fs');
+
+
 
 const server = http.createServer((req,res) => {
-    res.end('hello world welcome toe _ _');
-})
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    fs.readFile('page2.html',(err,data) => {
+        res.end(data);
+    });
+});
 
 
 server.listen(8080,() => {
