fe71a1127e0cf5198940155e0681f90aad0f7e22
muhid <shaikhmuhid165@gmail.com>
2023-09-25 10:34:45 +0530

    commited the code

--- page2.js
+++ page2.js
@@ -9,11 +9,165 @@ const form2 = document.querySelector(".form-container2");
 const skillAdd = document.querySelector(".add-addMore-skills");
 const form3 = document.querySelector(".form-container3");
 
+// CONNECTING THE FORM WITH THE TEMPLATE FROM HERE !!!!!!!!!!
+
 const firstNameInput = document.querySelector("#firstname_input");
 const firstNameOutput = document.querySelector("#firstname_output");
 
 const lastnameInput = document.querySelector("#lastname_input");
 const lastnameOutput = document.querySelector("#lastname_output");
+
+const phoneInput = document.querySelector("#phone_input");
+const phoneOutput = document.querySelector("#phone_output");
+
+const emailInput = document.querySelector("#email_input");
+const emailOutput = document.querySelector("#email_output");
+
+const jobTitleInput = document.querySelector("#jobTitle_input");
+const jobTitleOutput = document.querySelector("#jobTitle_output");
+
+const interestInput = document.querySelector("#interest_input");
+const interestOutput = document.querySelector("#interest_output");
+
+const addrInput = document.querySelector("#addr_input");
+const addrOutput = document.querySelector("#addr_output");
+
+const cityInput = document.querySelector("#city_input");
+const cityOutput = document.querySelector("#city_output");
+
+const pcodeInput = document.querySelector("#pcode_input");
+const pcodeOutput = document.querySelector("#pcode_output");
+
+const sttInput = document.querySelector("#stt_input");
+const sttOutput = document.querySelector("#stt_output");
+
+const cntryInput = document.querySelector("#cntry_input");
+const cntryOutput = document.querySelector("#cntry_output");
+
+const schlInput = document.querySelector("#schl_input");
+const schlOutput = document.querySelector("#schl_output");
+
+const lctInput = document.querySelector("#lct_input");
+const lctOutput = document.querySelector("#lct_output");
+
+const degInput = document.querySelector("#deg_input");
+const degOutput = document.querySelector("#deg_output");
+
+const fldInput = document.querySelector("#fld_input");
+const fldOutput = document.querySelector("#fld_output");
+
+const grdInput = document.querySelector("#grd_input");
+const grdOutput = document.querySelector("#grd_output");
+
+const sclwbstInput = document.querySelector("#sclwbst_input");
+const sclwbstoutput = document.querySelector("#sclwbst_output");
+
+const sclLinkInput = document.querySelector("#sclLink_input");
+const sclLinkoutput = document.querySelector("#sclLink_output");
+
+const skll1Input = document.querySelector("#skl_input1");
+const skill1output = document.querySelector("#skl_output1");
+
+const sklLvlInput1 = document.querySelector("#sklLvl_input1");
+const sklLvloutput1 = document.querySelector("#sklLvl_output1");
+
+
+
+skll1Input.addEventListener("input", (event) => {
+  skill1output.textContent = event.target.value;
+});
+
+sklLvlInput1.addEventListener("input", (event) => {
+  sklLvloutput1.textContent = event.target.value;
+});
+
+
+
+firstNameInput.addEventListener("input", (event) => {
+  firstNameOutput.textContent = event.target.value;
+});
+
+lastnameInput.addEventListener("input", (event) => {
+  lastnameOutput.textContent = event.target.value;
+});
+
+phoneInput.addEventListener("input",(event) => {
+  phoneOutput.textContent = event.target.value;
+});
+
+emailInput.addEventListener("input",(event) => {
+  emailOutput.textContent = event.target.value;
+});
+
+jobTitleInput.addEventListener("input",(event) => {
+  jobTitleOutput.textContent = event.target.value;
+});
+
+interestInput.addEventListener("input",(event) => {
+  interestOutput.textContent = event.target.value;
+});
+
+addrInput.addEventListener("input",(event) => {
+  addrOutput.textContent = event.target.value;
+});
+
+cityInput.addEventListener("input",(event) => {
+  cityOutput.textContent = event.target.value;
+});
+
+//n
+pcodeInput.addEventListener("input",(event) => {
+  pcodeOutput.textContent = event.target.value;
+});
+
+sttInput.addEventListener("input",(event) => {
+  sttOutput.textContent = event.target.value;
+});
+
+cntryInput.addEventListener("input",(event) => {
+  cntryOutput.textContent = event.target.value;
+});
+
+
+schlInput.addEventListener("input",(event) => {
+  schlOutput.textContent = event.target.value;
+});
+
+lctInput.addEventListener("input",(event) => {
+  lctOutput.textContent = event.target.value;
+});
+
+degInput.addEventListener("input",(event) => {
+  degOutput.textContent = event.target.value;
+});
+
+fldInput.addEventListener("input",(event) => {
+  fldOutput.textContent = event.target.value;
+});
+
+grdInput.addEventListener("input",(event) => {
+  grdOutput.textContent = event.target.value;
+});
+
+
+// sclwbstInput.addEventListener("input", (event) => {
+//   sclwbstoutput.textContent = event.target.value;
+// });
+
+
+// sclLinkInput.addEventListener("input", (event) => {
+//   sclLinkoutput.textContent = event.target.value;
+// });
+
+
+
+
+
+
+
+
+// id="sclLink_input"   id="sclwbst_input" 
+
 
 socialLinkAdd.addEventListener("click", () => {
   const socialElements = `<div class="input-container2">
@@ -30,13 +184,6 @@ socialLinkAdd.addEventListener("click", () => {
   form.insertAdjacentHTML("beforeend", socialElements);
 });
 
-firstNameInput.addEventListener("input", (event) => {
-  firstNameOutput.textContent = event.target.value;
-});
-
-lastnameInput.addEventListener("input", (event) => {
-  lastnameOutput.textContent = event.target.value;
-});
 
 const deleteSocialLink = (event) => {
   event.preventDefault();
@@ -87,6 +234,12 @@ const deleteskill = (event) => {
   event.preventDefault();
   event.target.parentNode.remove();
 };
+
+
+
+
+
+
 
 
 
